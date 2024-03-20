8fc04f4c36df6a6309785e7f69f2304b414a3b93
thawro <tomaszhawro.kontakt@gmail.com>
2024-03-20 21:12:15 +0100

    minor changes

--- scripts/prepare_coco.sh
+++ scripts/prepare_coco.sh
@@ -6,32 +6,32 @@ cd COCO
 
 mkdir images annotations
 
-echo "...Processing zip files (download zip -> unzip -> remove zip)..."
+echo "-> Processing zip files (download zip -> unzip -> remove zip)"
 
-echo "...Processing train2017 images..."
+echo "-> Processing train2017 images files"
 wget images.cocodataset.org/zips/train2017.zip
 num_train_files=`unzip -l train2017.zip | wc -l`
 unzip -o train2017.zip -d images | tqdm --desc "Extracting train files:" --unit files --unit_scale --total $num_train_files > /dev/null
 rm train2017.zip
 
-echo "...Processing val2017 images..."
+echo "-> Processing val2017 images files"
 wget images.cocodataset.org/zips/val2017.zip
 num_val_files=`unzip -l val2017.zip | wc -l`
 unzip -o val2017.zip -d images | tqdm --desc "Extracting val files:" --unit files --unit_scale --total $num_val_files > /dev/null
 rm val2017.zip
 
-echo "...Processing test2017 images..."
+echo "-> Processing test2017 images files"
 wget images.cocodataset.org/zips/test2017.zip
 num_test_files=`unzip -l test2017.zip | wc -l`
 unzip -o test2017.zip -d images | tqdm --desc "Extracting test files:" --unit files --unit_scale --total $num_test_files > /dev/null
 rm test2017.zip
 
-echo "...Processing trainval2017 annotations..."
+echo "-> Processing trainval2017 annotations"
 wget images.cocodataset.org/annotations/annotations_trainval2017.zip
 unzip annotations_trainval2017.zip
 rm annotations_trainval2017.zip
 
-
+echo "Data statistics:"
 echo "number of train samples: `find images/train2017 -name "*.jpg" | wc -l`"
 echo "number of val samples: `find images/val2017 -name "*.jpg" | wc -l`"
 echo "number of test samples: `find images/test2017 -name "*.jpg" | wc -l`"
--- scripts/prepare_imagenet.sh
+++ scripts/prepare_imagenet.sh
@@ -4,24 +4,24 @@ cd data
 
 num_files=`unzip -l imagenet-object-localization-challenge.zip | wc -l`
 
-echo Unzipping imagenet-object-localization-challenge.zip file: ${num_files} .JPEG files
+echo "-> Unzipping imagenet-object-localization-challenge.zip file: ${num_files} files"
 unzip -o imagenet-object-localization-challenge.zip | tqdm --desc "Extracting files:" --unit files --unit_scale --total $num_files > /dev/null
-echo "Unzipped imagenet-object-localization-challenge.zip file"
+echo "-> Unzipped imagenet-object-localization-challenge.zip file"
 
 rm imagenet-object-localization-challenge.zip
-echo "Removed imagenet-object-localization-challenge.zip file"
+echo "-> Removed imagenet-object-localization-challenge.zip file"
 
-echo "...Moving train, val and test images to data/ImageNet..."
+echo "-> Moving train, val and test images files to data/ImageNet"
 mkdir ImageNet
 
 mv ILSVRC/Data/CLS-LOC/train ImageNet/
-echo "Moved train files"
+echo "-> Moved train files"
 
 mv ILSVRC/Data/CLS-LOC/val ImageNet/
-echo "Moved val files"
+echo "-> Moved val files"
 
 mv ILSVRC/Data/CLS-LOC/test ImageNet/
-echo "Moved test files"
+echo "-> Moved test files"
 
 rm -rf ILSVRC
 rm LOC_sample_submission.csv
@@ -29,18 +29,19 @@ rm LOC_synset_mapping.txt
 rm LOC_train_solution.csv
 rm LOC_val_solution.csv
 
-echo "Removed redundant files and directories"
+echo "-> Removed redundant files and directories"
 
-echo "...Parsing val files to match train files structure..."
+echo "-> Parsing val files to match train files structure"
 cd ImageNet/val
 wget -qO- https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh | bash
 cd ..
 
-echo "...Downloading wordnet labels mapping..."
+echo "-> Downloading wordnet labels mapping"
 wget https://storage.googleapis.com/download.tensorflow.org/data/imagenet_class_index.json
 mv imagenet_class_index.json wordnet_labels.yaml
 
 
+echo "Data statistics:"
 echo "number of train samples: `find train/ -name "*.JPEG" | wc -l`"
 echo "number of val samples: `find val/ -name "*.JPEG" | wc -l`"
 echo "number of test samples: `find test/ -name "*.JPEG" | wc -l`"
