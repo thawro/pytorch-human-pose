66c16df8a669ebc6f5380a1c455c7d3d1559e3cc
thawro <tomaszhawro.kontakt@gmail.com>
2024-03-22 19:27:07 +0100

    fixed typehint in classification datamodule

--- src/classification/datamodule.py
+++ src/classification/datamodule.py
@@ -1,22 +1,21 @@
 """DataModule used to load DataLoaders"""
 
-from torchvision.datasets import ImageFolder
-
 from src.base.datamodule import DataModule
+from src.classification.datasets.imagenet import ImagenetClassificationDataset
 
 
 class ClassificationDataModule(DataModule):
-    train_ds: ImageFolder
-    val_ds: ImageFolder
-    test_ds: ImageFolder
+    train_ds: ImagenetClassificationDataset
+    val_ds: ImagenetClassificationDataset
+    test_ds: ImagenetClassificationDataset
 
 
 if __name__ == "__main__":
     from src.classification.transforms import ClassificationTransform
 
     transform = ClassificationTransform()
-    train_ds = ImageFolder("data/ImageNet/train", transform.train)
-    val_ds = ImageFolder("data/ImageNet/val", transform.inference)
+    train_ds = ImagenetClassificationDataset("data/ImageNet", "train", transform.train)
+    val_ds = ImagenetClassificationDataset("data/ImageNet", "val", transform.inference)
 
     datamodule = ClassificationDataModule(
         train_ds,
